5710d57bc90c8846b60ab1dda91a1d13840e3819
simonbinyamin <simonbinyamin@gmail.com>
2019-07-19 18:07:13 +0100

    Add project files.

---
+++ SignalR/Chat/ChatHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.Chat
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        /// <summary>
+        /// Template on how to create a private chat
+        /// </summary>
+        public async void PrivateChat(string message)
+        {
+            var t1 = new
+            {
+                fName="",
+                sName="",
+                responseConnId = ""
+            };
+
+            List<string> connIds = new List<string>();
+            connIds.Add(Context.ConnectionId);
+            connIds.Add(t1.responseConnId);
+
+            await Clients.Clients(connIds).SendAsync("ReceiveMessage", t1, message);
+
+        }
+    }
+}
---
+++ SignalR/Controllers/ChatController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.Chat;
+
+namespace SignalR.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index(string _id) => View((object)_id);
+       
+    }
+}
